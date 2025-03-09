9f823bf8efee52f9014bd252b4dac9991086d18e
tataalest <tataalest>
2025-03-09 13:18:24 +0700

    make hero section

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'includes/header.php' ?>
+<!-- Hero Section -->
+<section class="hero d-flex align-items-center justify-content-center text-center text-white"
+    data-aos="fade-in" data-aos-duration="1000">
+    <div class="hero-content">
+        <h3 data-aos="fade-in" data-aos-delay="300">Selamat Datang di</h3>
+        <h1 data-aos="zoom-in" data-aos-delay="500">CrunchyBite</h1>
+        <p data-aos="fade-in" data-aos-delay="700">Kami Menghidangkan Kelezatan Pastri & Kue dengan Sentuhan Istimewa</p>
+        <a href="/web-crunchybite/product.php"
+            class="btn-cta px-4 py-2 rounded-pill text-light mt-3" onclick="alert('halo saya putri suria lestari dengan nim 2311103077')"
+            data-aos="zoom-in" data-aos-delay="900">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path fill="#fff"
+                    d="M7 22q-.825 0-1.412-.587T5 20t.588-1.412T7 18t1.413.588T9 20t-.587 1.413T7 22m10 0q-.825 0-1.412-.587T15 20t.588-1.412T17 18t1.413.588T19 20t-.587 1.413T17 22M5.2 4h16.5l-4.975 9H8.1L7 15h12v2H3.625L6.6 11.6L3 4H1V2h3.25z" />
+            </svg> BELI SEKARANG
+        </a>
+    </div>
+</section>
